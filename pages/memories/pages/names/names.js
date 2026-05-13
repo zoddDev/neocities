d741b35d0ea97e0eaade4ccff80f1aa6360f22b7
Zodd <josiesala@gmail.com>
2026-05-14 01:46:15 +0200

    Add white noise to names page

--- pages/memories/pages/names/names.js
+++ pages/memories/pages/names/names.js
@@ -1,12 +1,14 @@
-import { AudioLoader } from "../../../../scripts/lib/musicLoader";
+import { AudioLoader } from "../../../../scripts/lib/musicLoader.js";
 
 export class Names {
   static SONG_URL =
     "https://cdn.jsdelivr.net/gh/zoddDev/neocities@main/resources/audios/music/white_noise.mp3";
 
   constructor() {
-    new AudioLoader(MemoriesPage.SONG_URL).play(true, 0.2);
+    new AudioLoader(Names.SONG_URL).play(true, 0.2);
   }
 
   init() {}
 }
+
+new Names().init();
